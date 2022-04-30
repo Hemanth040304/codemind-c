6e77dcf6ea0c9aab6ebedfd8bfe11a732c2db1f2
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-05-01 02:12:30 +0530

    #include<limits.h> #include<stdio.h> int main() {     int n,c=INT_MAX,count=0;     scanf("%d",&n);     int arr[n];     for(int i=0,d;i<n;i++)     {         d=0;         scanf("%d",&arr[i]);         int temp=arr[i];         while(temp)         {             d++;             temp/=10;         }         if(d<c){             c=d;         }     }     for(int i=0,d;i<n;i++)     {       d=0;         scanf("%d",&arr[i]);         while(arr[i])         {             d++;             arr[i]/=10;         }         if(d==c){             count++;         }     }     printf("%d",count); }

---
+++ Minimum_digit_elements.c
@@ -0,0 +1,36 @@
+#include<limits.h>
+#include<stdio.h>
+int main()
+{
+    int n,c=INT_MAX,count=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0,d;i<n;i++)
+    {
+        d=0;
+        scanf("%d",&arr[i]);
+        int temp=arr[i];
+        while(temp)
+        {
+            d++;
+            temp/=10;
+        }
+        if(d<c){
+            c=d;
+        }
+    }
+    for(int i=0,d;i<n;i++)
+    {
+      d=0;
+        scanf("%d",&arr[i]);
+        while(arr[i])
+        {
+            d++;
+            arr[i]/=10;
+        }
+        if(d==c){
+            count++;
+        }   
+    }
+    printf("%d",count);
+}
