6d0a65246954d07e192cbc9a01d13c316554a277
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-05-23 22:42:32 +0530

    #include<stdio.h> int prime(int n) {     if(n==1)     return 0;     for(int i=2;i<n/2;i++)     {         if(n%i==0)         return 0;     }     return 1; } int main() {     int n,c=0;     scanf("%d",&n);     for(int i=2;i<n;i++)     {         if(n%i==0)         {             if(prime(i))             {                 printf("%d ",i);                 c++;             }         }     }     if(c==0)     printf("-1"); }

--- Find_two_distinct_prime_numbers_with_given_product.c
+++ Find_two_distinct_prime_numbers_with_given_product.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
-int prime(int n){
+int prime(int n)
+{
     if(n==1)
-    {
-        return 0;
-    }
+    return 0;
     for(int i=2;i<n/2;i++)
     {
         if(n%i==0)
@@ -19,9 +18,10 @@ int main()
     {
         if(n%i==0)
         {
-            if(prime(i)){
-                c++;
+            if(prime(i))
+            {
                 printf("%d ",i);
+                c++;
             }
         }
     }
