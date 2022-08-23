5c33df1d3dc9b22d8be59bb59b8a7dfa7b502a6f
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-08-23 10:32:07 +0530

    #include<stdio.h> int main() {     int n,fact=0;     scanf("%d",&n);     for(int i=1;i<n;i++)     {         if(n%i==0)         {             fact+=i;         }     }     if(fact==n)     {         printf("True");     }     else{         printf("False");     } }

--- Perfect_Number.c
+++ Perfect_Number.c
@@ -1,15 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int n,sum=0;
+    int n,fact=0;
     scanf("%d",&n);
-    for(int i=1;i<=n/2;i++)
+    for(int i=1;i<n;i++)
     {
         if(n%i==0)
-        sum+=i;
+        {
+            fact+=i;
+        }
     }
-    if(sum==n)
-    printf("True");
-    else
-    printf("False");
+    if(fact==n)
+    {
+        printf("True");
+    }
+    else{
+        printf("False");
+    }
 }
