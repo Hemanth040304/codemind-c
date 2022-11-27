f88716e189658f8d041222f84083897559005ba9
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2022-11-27 12:27:08 +0530

    #include<stdio.h> #include<string.h> int main(){     char s[100];     scanf("%[^ ]s",s);     int l = strlen(s);     if(s[0]=='(' && s[l-1]==')'){         printf("True");     }     else{         printf("False");     } }

---
+++ Checking_valid_parenthesis_or_Not.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char s[100];
+    scanf("%[^
+]s",s);
+    int l = strlen(s);
+    if(s[0]=='(' && s[l-1]==')'){
+        printf("True");
+    }
+    else{
+        printf("False");
+    }
+}
